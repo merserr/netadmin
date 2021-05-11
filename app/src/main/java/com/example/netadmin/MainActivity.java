b8c99d53c61204d1c6b2b517b748d7157810c3a9
build <sermeretsky@gmail.com>
2021-05-11 15:52:52 +0300

    version 1.0

---
+++ app/src/main/java/com/example/netadmin/MainActivity.java
@@ -0,0 +1,405 @@
+package com.example.netadmin;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity  extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+
+
+
+
+    public final static String WIDGET_PREF = "widget_pref";
+    public final static String PASSWORD = "password";
+    String passwrd;
+
+    public static final String BROADCAST_ACTION = "com.example.herdcontrol";
+    public final static String MASSAGE = "inputMassage";
+    private static final String FILENAME = "herdcontroldata.csv";
+    private static final String LOG_TAG ="==MainActivity==" ;
+    private static final String FILENAME_SD = "herdcontroldata.csv";
+    private static final String DIR_SD = "Install";
+
+
+    String herddata = "";
+    final String ipaddress = "192.168.1.4";
+    final int port=1688;
+
+    //Создаем список вьюх которые будут создаваться
+    private List<View> allEds;
+    //счетчик чисто декоративный для визуального отображения edittext'ov
+    private int counter = 0;
+    BroadcastReceiver br;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        //    ============  add logo in toolbar  ===================
+        //    getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //    getSupportActionBar().setLogo(R.mipmap.lutron_control);
+        //    getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        //    ============  add logo in toolbar (2nd variant) ======
+        ActionBar menu = getSupportActionBar();
+        menu.setDisplayShowHomeEnabled(true);
+        menu.setIcon(R.mipmap.ic_launcher);
+
+
+
+        final SharedPreferences sp = getSharedPreferences(WIDGET_PREF, 0);
+        passwrd=sp.getString(MainActivity.PASSWORD, null);
+
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //toolbar.setLogo(R.mipmap.ic_launcher);        // add logo in toolbar
+        //setSupportActionBar(toolbar);                 // add name & calling menu
+
+
+
+        //getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_HOME_AS_UP | ActionBar.DISPLAY_USE_LOGO);
+
+
+
+//==========================================================================
+        // Receive massage from TCPService and make Toast
+        //==========================================================================
+        // create BroadcastReceiver
+
+        br = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String massage1 = intent.getStringExtra(MASSAGE);
+                //        Log.d(LOG_TAG, "onReceive: massage1 = " + massage1);
+                assert massage1 != null;
+                Processing(massage1);
+            }
+        };
+        // create filter for BroadcastReceiver
+        IntentFilter intFilt = new IntentFilter(BROADCAST_ACTION);
+        // registration (On) BroadcastReceiver
+        registerReceiver(br, intFilt);
+        //==========================================================================
+
+        Button button_save = (Button) findViewById(R.id.button_save);
+        Button button_read = (Button) findViewById(R.id.button_read);
+        Button button_readSD = (Button) findViewById(R.id.button_read_SD);
+        Button addButton = (Button) findViewById(R.id.button);
+        Button button_read_cisco = (Button) findViewById(R.id.button_read_cisco);
+
+        //инициализировали наш массив с edittext.aьи
+        allEds = new ArrayList<View>();
+
+        //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
+
+
+        /*
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createfield("00:11:22:33:44:55");
+            }
+        });
+*/
+
+        button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeFile();
+            }
+        });
+
+        button_readSD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readFileSD();
+            }
+        });
+
+        button_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readFile();
+            }
+        });
+
+        button_read_cisco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                get_data_from_server();
+            }
+        });
+
+    }
+
+
+
+    void writeFile() {
+        try {
+            Log.d(LOG_TAG, "writeFile");
+            // отрываем поток для записи
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+                    openFileOutput(FILENAME, MODE_PRIVATE)));
+            // пишем данные
+            bw.write(herddata);
+            // закрываем поток
+            bw.close();
+            Log.d(LOG_TAG, "Файл записан");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void readFile() {
+        try {
+            // открываем поток для чтения
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    openFileInput(FILENAME)));
+            String str = "";
+            // читаем содержимое
+            //Log.d(LOG_TAG, "readFile");
+            while ((str = br.readLine()) != null) {
+                // Log.d(LOG_TAG, str);
+                Log.d(LOG_TAG, "read File: "+str);
+                parseline(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    void readFileSD() {
+        // проверяем доступность SD
+        if (!Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            Log.d(LOG_TAG, "SD-карта не доступна: " + Environment.getExternalStorageState());
+            return;
+        }
+        // получаем путь к SD
+        File sdPath = Environment.getExternalStorageDirectory();
+        // добавляем свой каталог к пути
+        sdPath = new File(sdPath.getAbsolutePath() + "/" + DIR_SD);
+        //  формируем объект File, который содержит путь к файлу
+        Log.d(LOG_TAG, String.valueOf(sdPath));
+        File sdFile = new File(sdPath, FILENAME_SD);
+        try {
+            // открываем поток для чтения
+            Log.d(LOG_TAG, "==BufferedReader==");
+
+            BufferedReader br = new BufferedReader(new FileReader(sdFile));
+            String str = "";
+            // читаем содержимое
+            while ((str = br.readLine()) != null) {
+                Log.d(LOG_TAG, str);
+                herddata=herddata+str+"\n\r";
+            }
+            Log.d(LOG_TAG, herddata);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void get_data_from_server(){
+        //Toast.makeText(this, "Отслеживание переключения: " + (isChecked ? "on" : "off"), Toast.LENGTH_SHORT).show();
+
+        //String macaddresfromtextview = ((TextView)((View)(compoundButton.getParent())).findViewWithTag("id1")).getText().toString();
+        //Log.d(LOG_TAG, "======== if onCheckedChanged  =======: "+macaddresfromtextview);
+        //String command = isChecked ? "ON :" : "OFF:";
+        //String sending_command = command + macaddresfromtextview;
+        String sending_command = "info";
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, TCPService.class);
+        intent.putExtra("ipaddress", ipaddress);
+        intent.putExtra("port", port);
+        intent.putExtra("command", sending_command);
+        Log.d(LOG_TAG, "sending_command: "+sending_command);
+        MainActivity.this.startService(intent);
+
+
+    }
+
+    String[] parseline(String str){
+        String[] macaddroutput = new String[17]; //  ?????
+        // if (!inputMassage.matches("\\[(\\[\"-?\\d{10}\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\",\"-?\\d\\d?\\d?\\.\\d\"\\],?)+\\]")) { }
+
+        if (str.matches("[0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}.*")) {
+            Log.d(LOG_TAG, "input parseline Massage: "+ str);
+            //int dummyi;
+            //dummyi = str.indexOf("r001") + 8;
+            //macaddroutput = str.substring(dummyi, (dummyi + 1));
+            macaddroutput[0] = str.substring(0, (0 + 17));
+            macaddroutput[1] = str.substring(18);
+            Log.d(LOG_TAG, "macaddroutput = "+macaddroutput);
+            createfield(macaddroutput);
+        }
+
+        return macaddroutput;
+    }
+
+
+    void createfield(String[] textfield){
+        final LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        counter++;
+        //берем наш кастомный лейаут находим через него все наши кнопки и едит тексты, задаем нужные данные
+        final View view = getLayoutInflater().inflate(R.layout.custom_edittext_layout, null);
+
+        TextView text = (TextView) view.findViewById(R.id.editText);
+        text.setText(textfield[0]);
+     //   text.setText("192.168.100.222");
+
+        TextView text2 = (TextView) view.findViewById(R.id.editText2);
+        text2.setText(textfield[1]);
+     //   text2.setText("1122.3344.5566");
+
+
+        TextView text3 = (TextView) view.findViewById(R.id.editText3);
+        text3.setText(textfield[2]);
+     //   text3.setText("111");
+
+
+        TextView text4 = (TextView) view.findViewById(R.id.editText4);
+        text4.setText(textfield[3]);
+     //  text4.setText("manufacture");
+
+
+        TextView text5 = (TextView) view.findViewById(R.id.editText5);
+        text5.setText(textfield[4]);
+     //   text5.setText("name-name");
+
+
+
+        //добавляем все что создаем в массив
+        allEds.add(view);
+        //добавляем елементы в linearlayout
+        linear.addView(view);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        //Toast.makeText(this, "Отслеживание переключения: " + (isChecked ? "on" : "off"), Toast.LENGTH_SHORT).show();
+
+        String macaddresfromtextview = ((TextView)((View)(compoundButton.getParent())).findViewWithTag("id1")).getText().toString();
+        //Log.d(LOG_TAG, "======== if onCheckedChanged  =======: "+macaddresfromtextview);
+        String command = isChecked ? "ON :" : "OFF:";
+        String sending_command = command + macaddresfromtextview;
+
+        Intent intent = new Intent();
+        intent.setClass(MainActivity.this, TCPService.class);
+        intent.putExtra("ipaddress", ipaddress);
+        intent.putExtra("port", port);
+        intent.putExtra("command", sending_command);
+        Log.d(LOG_TAG, "sending_command: "+sending_command);
+        MainActivity.this.startService(intent);
+
+    }
+
+    void Processing(String inputMassage){
+    //    Toast.makeText(MainActivity.this, inputMassage, Toast.LENGTH_LONG).show();
+        //Log.d(LOG_TAG, "input Processing Massage1: "+ inputMassage);
+        if (inputMassage.matches("\\{\\{.*\\}\\}")) {
+
+            String line[] = inputMassage.split("\\}\\{");  // по пробелу
+            String clients[][] = new String[line.length][5];
+            int count=0;
+            while (count < line.length){
+                String subline[] = line[count].split(":");
+                clients[count][0] = subline[0];
+                clients[count][1] = subline[1];
+                clients[count][2] = subline[2];
+                clients[count][3] = subline[3];
+                clients[count][4] = subline[4];
+        //        Log.d(LOG_TAG, clients[count][0]+"   "+clients[count][1]+"   "+clients[count][2]+"   "+clients[count][3]+"   "+clients[count][4]);
+                createfield(subline);
+                count++;
+            }
+
+        } else if (inputMassage.matches("No connect to .*")) {
+            Toast.makeText(MainActivity.this, inputMassage, Toast.LENGTH_LONG).show();
+        } else if (inputMassage.matches("\\{\"cli\": \"NOT\".*")) {
+            Toast.makeText(MainActivity.this, "Client not ready", Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the main_menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings:
+                //your action
+                Log.d(LOG_TAG, "===action_settings===");
+                setContentView(R.layout.config);
+                final EditText edit_text_password = (EditText) findViewById(R.id.text_password);
+                final SharedPreferences sp = getSharedPreferences(WIDGET_PREF, 0);
+                edit_text_password.setText(sp.getString(MainActivity.PASSWORD, null));
+
+                Button button_ok = (Button) findViewById(R.id.button_ok);
+                button_ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        Log.d(LOG_TAG, "save_password");
+                        SharedPreferences.Editor editor = sp.edit();
+                        editor.putString(PASSWORD, edit_text_password.getText().toString());
+                        editor.commit();
+
+
+                    }
+                });
+                break;
+            case R.id.action_about:
+                //your action
+                Log.d(LOG_TAG, "===action_about===");
+                setContentView(R.layout.about);
+            break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/netadmin/TCPService.java
@@ -0,0 +1,142 @@
+package com.example.netadmin;
+
+import android.annotation.SuppressLint;
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import static com.example.netadmin.MainActivity.MASSAGE;
+
+public class TCPService extends Service {
+
+    final String LOG_TAG = "==TCPService==";
+    String ipaddress = "0.0.0.0";
+    int port = 1;
+    String command = "";
+    String sendcommand = "ooo";
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        if(intent!=null) {
+            ipaddress = intent.getStringExtra("ipaddress");
+            port = intent.getIntExtra("port", 2);
+            command = intent.getStringExtra("command");
+        }
+
+        new TCPConnect(ipaddress, port, command).execute();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    class TCPConnect extends AsyncTask<Void, Integer, Void> {
+
+        boolean connect=false;
+        final int port;
+        final String ipaddress;
+        final String sendstring;
+        String inputMassage;
+        Socket socket = null;
+        DataOutputStream dataOutputStream = null;
+        DataInputStream dataInputStream = null;
+        BufferedReader mBufferIn = null;
+
+        public TCPConnect(String ipaddress_in, int port_in, String command) {
+            // TODO Auto-generated constructor stub
+            ipaddress = ipaddress_in;
+            port=port_in;
+            //Log.d(LOG_TAG, "command: "+command);
+            // ---------- Проверка данных -------------
+            //    KBP, [2:4:22], 2
+            if (command.matches("O[NF].:[0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}[-:][0-9A-Fa-f]{2}.*")) {
+                sendstring = "\r\n"+command+"\r\n";
+            }else{
+                sendstring="\r\ninfo\r\n";
+            }
+            Log.d(LOG_TAG, "sendstring: "+sendstring);
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Log.d(LOG_TAG, "doInBackground sendstring: "+sendstring);
+
+            //Log.d(LOG_TAG, "sending comand to address "+ipaddress+" port "+port);
+
+            //sendstring="\r\ninfo\r\n";
+
+            try {
+                socket = new Socket();
+                socket.setSoTimeout(10000);
+                socket.connect(new InetSocketAddress(ipaddress, port), 1000);
+                connect=socket.isConnected();
+
+                dataInputStream = new DataInputStream(socket.getInputStream()); //read from client through inputstream
+
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                //    dataOutputStream.writeBytes(" ");
+                dataOutputStream.writeBytes(sendstring);
+
+                mBufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //read input massage from buffer
+                inputMassage = mBufferIn.readLine();
+
+//  delay 5000ms
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (InterruptedException ex) {
+//                    ex.printStackTrace();
+//                }
+
+                dataInputStream.close();
+                dataOutputStream.close();
+                socket.close();
+                //               Log.d(LOG_TAG, "--socket.close()--");
+
+            }catch (UnknownHostException e) {
+                e.printStackTrace();
+                Log.d(LOG_TAG, "--UnknownHostException--");
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.d(LOG_TAG, "--IOException--");
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            if (!connect) {
+                Log.d(LOG_TAG, "NOT CONNECT");
+
+                // send massage "No connect" to MainActivity
+                Intent intent2 = new Intent(MainActivity.BROADCAST_ACTION);
+                intent2.putExtra(MASSAGE, "No connect to "+ipaddress+":"+port);
+                sendBroadcast(intent2);
+            } else {
+                Log.d(LOG_TAG, "inputMassage2: "+ inputMassage);
+
+                Intent intent1 = new Intent(MainActivity.BROADCAST_ACTION);
+                intent1.putExtra(MASSAGE, inputMassage);
+                sendBroadcast(intent1);
+            }
+            connect=false;
+        }
+    }
+
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+}
